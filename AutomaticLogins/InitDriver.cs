6373deb78476aca482253470f79090a0a0000f7f
deb <jagadeba_d@WS106468>
2018-09-27 16:39:29 +1000

    intial commit

---
+++ AutomaticLogins/InitDriver.cs
@@ -0,0 +1,124 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+namespace AutomaticLogins
+{
+    public class InitDriver
+    {
+
+        private static IWebDriver webDriver;
+
+        public static void Init(string driverTyper, string userId, string pwd, AppVals appVals, string url = null, string envVal = null)
+        {
+            try
+            {
+                var test1 = url;
+                var test2 = envVal;
+                string path = Path.Combine(Environment.CurrentDirectory, @"Drivers\");
+                int WaitTimeInSeconds = 2000;
+                //string application = 
+                switch (driverTyper.ToLower())
+                {
+                    case "chrome":
+                        webDriver = new ChromeDriver(path);
+                        break;
+
+                    case "firefox":
+                        webDriver = new FirefoxDriver(path);
+                        break;
+
+                    case "ie":
+                        webDriver = new InternetExplorerDriver(path);
+                        break;
+                    default:
+                        throw new NotSupportedException("This browser is not supportd");
+                       
+                }
+                var findBy = appVals.findBy;
+                By userIdFindBy = null;
+              
+                By pwdFindBy = null;
+                By selectPathFindBy = null;
+                By selectValFindBy = null;
+                By submitFindBy = null;
+
+                switch (findBy)
+                {
+                    case "css":
+                        userIdFindBy = By.CssSelector(appVals.userId);
+                        pwdFindBy = By.CssSelector(appVals.password);
+                        selectPathFindBy = By.CssSelector(appVals.selectPath);
+                        selectValFindBy = By.CssSelector(appVals.selectBy);
+                        submitFindBy = By.CssSelector(appVals.submit);
+                        break;
+                    case "id":
+                        userIdFindBy = By.Id(appVals.userId);
+                        pwdFindBy = By.Id(appVals.password);
+                        selectPathFindBy = By.Id(appVals.selectPath);
+                        selectValFindBy = By.Id(appVals.selectBy);
+                        submitFindBy = By.Id(appVals.submit);
+                        break;
+                    case "xpath":
+                        userIdFindBy = By.XPath(appVals.userId);
+                        pwdFindBy = By.XPath(appVals.password);
+                        selectPathFindBy = By.XPath(appVals.selectPath);
+                        selectValFindBy = By.XPath(appVals.selectBy);
+                        submitFindBy = By.XPath(appVals.submit);
+
+                        break;
+
+                    default:
+                        break;
+                }
+                if (url == null)
+                {
+                    webDriver.Navigate().GoToUrl(appVals.url);
+                }
+                else
+                {
+                    webDriver.Navigate().GoToUrl(url);
+                }
+              
+                Thread.Sleep(WaitTimeInSeconds);
+                webDriver.Manage().Window.Maximize();
+               
+                webDriver.FindElement(userIdFindBy).SendKeys(userId);
+                webDriver.FindElement(pwdFindBy).SendKeys(pwd);
+
+                if(appVals.selectEnv)
+                {
+                    webDriver.FindElement(selectPathFindBy).Click();
+                    var items = webDriver.FindElements(selectValFindBy);
+                    string textTofind = envVal == null ? appVals.defaultSelectEnv : envVal;
+                    foreach (var item in items)
+                    {
+                        if (item.Text.Contains(textTofind))
+                        {
+                            item.Click();
+                            break;
+                        }
+                    }
+                }
+                webDriver.FindElement(submitFindBy).Click();
+                Thread.Sleep(WaitTimeInSeconds);
+    
+                //webDriver.Close();
+                //webDriver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ AutomaticLogins/Program.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace AutomaticLogins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string browserType, app, userId, pwd, url = null, envToSelect = null;
+
+
+            try
+            {
+            
+                if (args[0].ToLower(CultureInfo.InvariantCulture).IndexOf("help", System.StringComparison.Ordinal) >= 0)
+                {
+                    Console.WriteLine("--------------------Argument Details-------------------------\n");
+                    string[] parameterNumber = { "1.", "2.", "3.", "4.","5."};
+                    string[] parameterName = { "Browser", "Application", "UserId", "Password", "Url" };
+                    string[] parameterOption = { "Mandatory", "Mandatory", "Mandatory", "Mandatory", "Optional" };
+                    string[] possibleValues = { "Chrome, Firefox, IE", "NA", "NA", "NA", "NA" };
+
+                    Console.WriteLine("{0,-3} {1,10} {2,10} {3,10}\n", "No.", "Name", "Option", "Values");
+                    for (int ctr = 0; ctr < parameterNumber.Length; ctr++)
+                    {
+                        Console.WriteLine("{0,-3} {1,10:N1} {2,10:N2} {3,10:N3}", parameterNumber[ctr], parameterName[ctr], parameterOption[ctr], possibleValues[ctr]);
+                    }
+                       
+                  
+                }
+                else
+                {
+                    var parsedArgs = args.Select(s => s.Split(new[] { '-' })).ToDictionary(s => s[0].ToLower(), s => s[1]);
+                    if (args.Length < 4)
+                    {
+                        throw new ArgumentException("Missing arguments");
+                    }
+                    else
+                    {
+                      
+                        try
+                        {
+                             browserType = parsedArgs.Single(x => x.Key == "browser").Value;
+                        }
+                        catch (Exception)
+                        {
+
+                            throw new ArgumentException("browser type is missing, for example command should be browser:chrome");
+                        }
+
+
+                        try
+                        {
+                             app = parsedArgs.Single(x => x.Key == "app").Value;
+                        }
+                        catch (Exception)
+                        {
+
+                            throw new ArgumentException("app type is missing, for example command should be app:jira");
+                        }
+
+
+                        try
+                        {
+                             userId = parsedArgs["userid"];
+                        }
+                        catch (Exception)
+                        {
+
+                            throw new ArgumentException("user id is missing, for example command should be userid:someuserid");
+                        }
+
+
+                        try
+                        {
+                             pwd = parsedArgs["pwd"];
+                        }
+                        catch (Exception)
+                        {
+
+                            throw new ArgumentException("password is missing, for example command should be pwd:somepassword");
+                        }
+
+                        try
+                        {
+                            if(args.Length >= 5)
+                            {
+                                if(parsedArgs.ContainsKey("url"))
+                                {
+                                    url = parsedArgs["url"];
+                                }
+                                //else
+                                //{
+                                //    url = "";
+                                //}
+                            }
+                                //url = args.Length >= 5 ? parsedArgs["url"] : "";
+                        }
+                        catch (Exception)
+                        {
+
+                            throw new ArgumentException("url is optional but if passed it should be url:someUrl");
+                        }
+
+                        try
+                        {
+                            if (args.Length >= 5)
+                            {
+                                if (parsedArgs.ContainsKey("env"))
+                                {
+                                    envToSelect = parsedArgs["env"];
+                                }
+                     
+                            }
+                            //envToSelect = args.Length >= 5 ? parsedArgs["env"] : "";
+                        }
+                        catch (Exception)
+                        {
+
+                            throw new ArgumentException("env is optional but if passed it should be env:exact name of the env from list");
+                        }
+
+
+
+                        var builder = new ConfigurationBuilder().SetBasePath
+                                            (Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json");
+                        var Configuration = builder.Build();
+                        AppVals appVals = new AppVals();
+                        string sectionToGetVals = "Apps:" + app;
+
+                        if (!Configuration.GetSection(sectionToGetVals).Exists())
+                        {
+
+                            throw new ArgumentException(app + " is not a valid application");
+                        }
+                        Configuration.GetSection(sectionToGetVals).Bind(appVals);
+                        //if (url == null)
+                        //{
+                        //    InitDriver.Init(browserType, userId, pwd, appVals);
+                        //}
+                        //else
+                        //{
+                            InitDriver.Init(driverTyper: browserType, userId: userId, pwd: pwd, appVals:appVals, url:url, envVal:envToSelect);
+                        //}
+                        //if (appVals.selectEnv)
+                        //{
+
+                        //}
+                       
+                    }
+                }
+                
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+            //catch if any other exception 
+            catch (Exception moreExcpetion)
+            {
+                Console.WriteLine(moreExcpetion.ToString());
+            }
+
+        }
+    }
+
+    public class AppVals
+    {
+        public string alias { get; set; }
+        public string url { get; set; }
+        public bool selectEnv { get; set; }
+        public string findBy { get; set; }
+        public string userId { get; set; }
+        public string password { get; set; }
+        public string selectPath { get; set; }
+        public string selectBy { get; set; }
+        public string defaultSelectEnv { get; set; }
+        public string submit { get; set; }
+    }
+}
